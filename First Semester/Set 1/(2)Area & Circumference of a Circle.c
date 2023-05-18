9f63c82f7deda9b9ded7a40859be83f5994e014d
Md.Abdullah Al Rakib <122741559+iamrakib087@users.noreply.github.com>
2023-05-18 19:47:16 +0600

    Update and rename (2) Circumference of a Circle.c to (2)Area & Circumference of a Circle.c

--- First Semester/Set 1/(2)Area & Circumference of a Circle.c
+++ First Semester/Set 1/(2)Area & Circumference of a Circle.c
@@ -5,6 +5,8 @@ int main(){
 float r,c;
 printf("Enter The Value of r: ");
 scanf("%f",&r);
+a=(pi*r*r);
 c=(2*pi*r);
+printf("\nThe Area of The Circle Is: %.2f",a);
 printf("\nThe Circumference of The Circle Is: %.2f",c);
 return 0;}
